78577c9d19b1385a6cbbacbb267cc5affd72941e
thedarkginger <jp.gooderham@gmail.com>
2014-11-26 22:26:46 -0500

    initial commit for hundred buck project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hundredbuck::Application.config.secret_key_base = '7e798435628ee12a4601b4ae43793e4ab9a8538610b032e81250d92a9ad44075e33eba9bb5fc2cc390783a2503bb5cdb4c3b788016ab92863e26e6c146f3acd3'
